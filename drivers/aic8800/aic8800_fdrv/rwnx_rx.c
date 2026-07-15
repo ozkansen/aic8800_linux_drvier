18c02b363a7acb627cf8c5b6e3a1195006440d11
Özkan ŞEN <ozkansen@gmail.com>
2026-07-15 14:31:53 +0300

    fix: cfg80211_rx_spurious_frame link_id esigi 6.7.0'e yukseltildi
    - Eski: link_id parametresi icin KERNEL_VERSION(6, 0, 0) esigi
    - Yeni: link_id parametresi icin KERNEL_VERSION(6, 7, 0) esigi
    - link_id parametresi upstream kernel 6.7'de eklendi
    - Rockchip vendor kernel 6.1.115'de bu parametre henuz yok
    - Eski esik nedeniyle 4-parametreli versiyon kullaniliyordu (hata)

--- drivers/aic8800/aic8800_fdrv/rwnx_rx.c
+++ drivers/aic8800/aic8800_fdrv/rwnx_rx.c
@@ -2073,7 +2073,7 @@ check_len_update:
         hdr = (struct ieee80211_hdr *)(skb->data + msdu_offset);
         rwnx_vif = rwnx_rx_get_vif(rwnx_hw, hw_rxhdr->flags_vif_idx);
         if (rwnx_vif) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
             cfg80211_rx_spurious_frame(rwnx_vif->ndev, hdr->addr2, -1, GFP_ATOMIC);
 #else
             cfg80211_rx_spurious_frame(rwnx_vif->ndev, hdr->addr2, GFP_ATOMIC);
